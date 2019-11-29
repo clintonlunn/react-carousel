f4523b9c6af56b4ded03a6b311d0045e7d3282c6
Clinton Lunn <clintonlunn@Clintons-MacBook-Pro.local>
2019-11-29 11:28:47 -0500

    Updates

---
+++ precache-manifest.b923e8ad025cc751e033ba5c47d13032.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0de930b11a67dd5316abce45b5e3ef",
+    "url": "/react-carousel/index.html"
+  },
+  {
+    "revision": "b506e760b9f6b4b14f6f",
+    "url": "/react-carousel/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "ff085e1f24cd57aee218",
+    "url": "/react-carousel/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b506e760b9f6b4b14f6f",
+    "url": "/react-carousel/static/js/2.f8dae853.chunk.js"
+  },
+  {
+    "revision": "ff085e1f24cd57aee218",
+    "url": "/react-carousel/static/js/main.3b2f237c.chunk.js"
+  },
+  {
+    "revision": "bb8ebdfc4f20a934e613",
+    "url": "/react-carousel/static/js/runtime-main.ef1ed1f6.js"
+  }
+]);
